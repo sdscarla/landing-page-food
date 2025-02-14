961735f3f6aaac66e4ebfbd62264bebf34757356
linlin <sdsantoscarla@gmail.com>
2025-02-14 19:11:04 -0300

    Header finalizado

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
     $('#mobile_btn').on('click', function () {
         $('#mobile_menu').toggleClass('active');
+        $('#mobile_btn').find('i').toggleClass('fa-x'); //.find('i') é para ele encontrar o icone do botão para ai ele trocar
     });
 });
